bd605160fd30a8047b39a6b260e63c2b1c3d12b3
sylvieshimwauwase <slvshimwa@gmail.com>
2023-10-30 17:15:22 +0200

    changes

--- api/v1/index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from api.v1.views import app_views
-from flask import Fask, jsonify
-from models import storage
-
-@app_views.route('status', methods=['GET'], strict_slashes=False)
-def get_status():
-    return jsonify({"status"}: {"OK"})
