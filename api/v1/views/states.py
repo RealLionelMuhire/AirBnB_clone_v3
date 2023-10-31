07c502eb591840bb282630f997974864a46126fa
RealLionelMuhire <muhirelionel@gmail.com>
2023-10-31 10:28:40 +0200

    fixing a typo

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,6 +2,6 @@
 """ handling routes for state"""
 
 from flask import jsonify, request, abort
-from ap1.v1.views import app_views
+from api.v1.views import app_views
 from models import storage
 from models.state import State
